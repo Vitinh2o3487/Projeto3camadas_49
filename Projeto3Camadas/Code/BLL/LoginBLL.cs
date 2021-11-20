5adb7acc980d98bbdf2790d2ed32b6db7e154c48
Vitinh2o3487 <80173204+Vitinh2o3487@users.noreply.github.com>
2021-11-19 22:14:44 -0300

    Add files via upload

---
+++ Projeto3Camadas/Code/BLL/LoginBLL.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+
+
+using Projeto3Camadas.Code.DTO; 
+using Projeto3Camadas.Code.DAL; 
+
+
+namespace Projeto3Camadas.Code.BLL
+{
+    class LoginBLL
+    {
+
+       
+        AcessoBancoDados conexao = new AcessoBancoDados();
+        string tabela = "tbl_login";
+
+        public bool RealizarLogin(LoginDTO login)    
+        {
+            string sql = $"select * from {tabela} where email='{login.Email}' and senha='{login.Senha}'";
+            DataTable dt = conexao.ExecutarConsulta(sql);
+
+            if (dt.Rows.Count > 0)
+                return true;            
+            else
+                return false;
+        }
+
+        public string RetornarSenha(LoginDTO login)    
+        {
+            string sql = $"select * from {tabela} where email='{login.Email}'";
+            DataTable dt = conexao.ExecutarConsulta(sql);
+
+            if (dt.Rows.Count > 0)
+                return dt.Rows[0]["senha"].ToString();
+            else
+                return "false";
+        }
+
+    }
+}
---
+++ Projeto3Camadas/Code/BLL/MedicamentoBLL.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using Projeto3Camadas.Code.DTO; 
+using Projeto3Camadas.Code.DAL; 
+
+namespace Projeto3Camadas.Code.BLL
+{
+    class MedicamentoBLL
+    {
+
+        
+        AcessoBancoDados conexao = new AcessoBancoDados();
+        string tabela = "tbl_medicamento";
+
+
+       
+        public void Inserir (MedicamentoDTO medDto)
+        {
+           
+            string inserir = $"insert into {tabela} values(null,'{medDto.Medicamento}','{medDto.Composicao}')";
+            conexao.ExecutarComando(inserir);
+        }
+
+        public DataTable Listar()    
+        {
+            string sql = $"select * from {tabela} order by id;";
+            return conexao.ExecutarConsulta(sql);
+        }
+
+        public void Editar(MedicamentoDTO medDto)
+        {
+            string alterar = $"update {tabela} set medicamento = '{medDto.Medicamento}', composicao = '{medDto.Composicao}' where id = '{medDto.Id}';";
+            conexao.ExecutarComando(alterar);
+        }
+
+
+        public void Excluir(MedicamentoDTO medDto)
+        {
+            string excluir = $"delete from {tabela} where id = '{medDto.Id}';";
+            conexao.ExecutarComando(excluir);
+        }
+    }
+}
